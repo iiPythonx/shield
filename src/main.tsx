d6c204b534a7d01a9688131e83ff5d8fb7398e94
iiPython <ben@iipython.dev>
2026-08-19 23:49:37 -0500

    feat: copyright info

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { render } from "preact";
-import { useRef } from "preact/hooks";
+import { useRef, useState } from "preact/hooks";
 import { parse } from "jsonc-parser";
 
 import { Modal } from "./components/modal";
@@ -13,6 +13,7 @@ const form_data = Object.values(import.meta.glob("./assets/forms/*.jsonc", { que
 
 export function App() {
     const [currentForm, setForm] = useForm();
+    const [copyrightOpen, setCopyrightOpen] = useState<boolean>(false);
     const dropdownReference = useRef<HTMLSelectElement | null>(null);
 
     const continueWithSelectedType = () => {
@@ -25,15 +26,26 @@ export function App() {
         setForm(form);
     }
 
-    return currentForm ? <Assessment form = {currentForm} /> : <Modal title = "Assessment Setup">
-        <p>Please select the type of assessment you're conducting:</p>
-        <select value = {""} ref = {dropdownReference}>
-            {form_data.map((form) => {
-                return <option value = {form.id}>{form.source} - {form.name}</option>;
-            })}
-        </select>
-        <button onClick = {continueWithSelectedType}>Continue</button>
-    </Modal>;
+    return <>
+        {
+            currentForm ? <Assessment /> : <Modal title = "Assessment Setup">
+                <p>Please select the type of assessment you're conducting:</p>
+                <select value = {""} ref = {dropdownReference}>
+                    {form_data.map((form) => {
+                        return <option value = {form.id}>{form.source} - {form.name}</option>;
+                    })}
+                </select>
+                <button onClick = {continueWithSelectedType}>Continue</button>
+            </Modal>
+        }
+        <footer onClick = {() => setCopyrightOpen(true)}>&copy; 2026</footer>
+        {copyrightOpen && <Modal title = "Copyright & Credits">
+            <p>Copyright &copy; 2026 Benjamin O'Brien</p>
+            <p>Designed by the Mississippi State University Cybersecurity Assessments Program</p>
+            <p>Made possible by <a href = "https://vite.dev">vite</a> and <a href = "https://preactjs.com">preact</a></p>
+            <button onClick = {() => setCopyrightOpen(false)}>Close</button>
+        </Modal>}
+    </>;
 }
 
 render(<App />, document.getElementById("content")!);
